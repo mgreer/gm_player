c79559245641f3f2ad12fedc40099921e3097a1d
Michael Greer <michael.greer@gmail.com>
2013-05-14 15:31:45 -0700

    initial

---
+++ player_controller.js
@@ -0,0 +1,48 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+// You may study, modify, and use this example for any purpose.
+// Note that this example is provided "as is", WITHOUT WARRANTY
+// of any kind either expressed or implied.
+
+
+var PlayerController = function() {
+  this.contentPlayer = document.getElementById('content');
+  this.adContainer = document.getElementById('adcontainer');
+
+  this.width = 640;
+  this.height = 360;
+
+  this.adPlaying = false;
+};
+
+PlayerController.prototype.setAdPlaying = function(adPlaying) {
+  this.adPlaying = adPlaying;
+};
+
+PlayerController.prototype.preloadContent = function(contentLoadedAction) {
+  // If this is the initial user action on iOS or Android device,
+  // simulate playback to enable the video element for later program-triggered
+  // playback.
+  if (this.isMobilePlatform()) {
+    this.contentPlayer.addEventListener(
+        'loadedmetadata',
+        contentLoadedAction,
+        false);
+    this.contentPlayer.load();
+  } else {
+    contentLoadedAction();
+  }
+};
+
+PlayerController.prototype.play = function() {
+  this.contentPlayer.play();
+};
+
+PlayerController.prototype.pause = function() {
+  this.contentPlayer.pause();
+};
+
+PlayerController.prototype.isMobilePlatform = function() {
+  return this.contentPlayer.paused &&
+      (navigator.userAgent.match(/(iPod|iPhone|iPad)/) ||
+       navigator.userAgent.toLowerCase().indexOf('android') > -1);
+};
